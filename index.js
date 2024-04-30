297e4f9e1cd0fcbc3eeec68a1f14c3862b893663
CoBaX756 <xab756@gmail.com>
2024-04-30 12:37:37 +0200

    aaa

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+
 mongoose.connect('mongodb://localhost/Netalmix', { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 if (!db) {
